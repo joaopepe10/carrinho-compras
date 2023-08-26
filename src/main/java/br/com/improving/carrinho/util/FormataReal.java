b9418f0d09953a596047f32b223a5f24beb06301
João Perdro Pires <joaopedropires61@gmail.com>
2023-08-25 21:56:17 -0300

    Feat: Formata big decimal para real

---
+++ src/main/java/br/com/improving/carrinho/util/FormataReal.java
@@ -0,0 +1,12 @@
+package br.com.improving.carrinho.util;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class FormataReal {
+	static NumberFormat formatandoValores = new DecimalFormat("R$ #,##0.00");
+	public static String bigDecimalToString(BigDecimal valor){
+		return formatandoValores.format(valor);
+	}
+}
