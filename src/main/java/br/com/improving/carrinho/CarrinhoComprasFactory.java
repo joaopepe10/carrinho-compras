174f3930427c39258eaf3ef0c509b0380d81eb88
João Perdro Pires <joaopedropires61@gmail.com>
2023-08-31 15:38:50 -0300

    Feat: Adiciona metodo de validar ID

--- src/main/java/br/com/improving/carrinho/CarrinhoComprasFactory.java
+++ src/main/java/br/com/improving/carrinho/CarrinhoComprasFactory.java
@@ -1,34 +1,50 @@
-/*
+
 package br.com.improving.carrinho;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-*/
+import br.com.improving.usuario.Cliente;
+
+
 /**
  * Classe responsável pela criação e recuperação dos carrinhos de compras.
- *//*
+ */
 
 public class CarrinhoComprasFactory {
 
-	public CarrinhoComprasFactory() {
+	private List<Cliente> clientes = new ArrayList<>();
+	private CarrinhoComprasFactory() {
 	}
 
-    */
-/**
+
+	/**
      * Cria e retorna um novo carrinho de compras para o cliente passado como parâmetro.
      *
      * Caso já exista um carrinho de compras para o cliente passado como parâmetro, este carrinho deverá ser retornado.
      *
      * @param identificacaoCliente
      * @return CarrinhoCompras
-     *//*
+     */
 
     public CarrinhoCompras criar(String identificacaoCliente) {
+		isCliente(identificacaoCliente)
+				.orElseThrow(()-> new RuntimeException("Id de cliente invalido!"));
 
+		return new CarrinhoCompras();
     }
 
-    */
-/**
+	private Optional<Boolean> isCliente(String identificacaoCliente){
+		return Optional.of(getClientes().stream()
+				.anyMatch(i -> i.getId().equals(identificacaoCliente)));
+	}
+
+
+
+
+	/**
      * Retorna o valor do ticket médio no momento da chamada ao método.
      * O valor do ticket médio é a soma do valor total de todos os carrinhos de compra dividido
      * pela quantidade de carrinhos de compra.
@@ -36,24 +52,32 @@ public class CarrinhoComprasFactory {
      * 0-4 deve ser arredondado para baixo e 5-9 deve ser arredondado para cima.
      *
      * @return BigDecimal
-     *//*
+     */
 
     public BigDecimal getValorTicketMedio() {
-
+		return new BigDecimal(0);
     }
 
-    */
-/**
+
+	/**
      * Invalida um carrinho de compras quando o cliente faz um checkout ou sua sessão expirar.
      * Deve ser efetuada a remoção do carrinho do cliente passado como parâmetro da listagem de carrinhos de compras.
      *
      * @param identificacaoCliente
      * @return Retorna um boolean, tendo o valor true caso o cliente passado como parämetro tenha um carrinho de compras e
      * e false caso o cliente não possua um carrinho.
-     *//*
+     */
 
     public boolean invalidar(String identificacaoCliente) {
-
+		return true;
     }
+
+	private List<Cliente> getClientes() {
+		return clientes;
+	}
+
+	private void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
+	}
 }
-*/
+
---
+++ src/main/java/br/com/improving/usuario/Cliente.java
@@ -0,0 +1,76 @@
+package br.com.improving.usuario;
+
+import java.util.Objects;
+
+import br.com.improving.carrinho.CarrinhoCompras;
+
+public class Cliente {
+	private String id;
+	private String nome;
+	private String sobrenome;
+	private CarrinhoCompras carrinho;
+
+	public Cliente(String id, String nome, String sobrenome){
+		this.id = id;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+	}
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public CarrinhoCompras getCarrinho() {
+		return carrinho;
+	}
+
+	public void setCarrinho(CarrinhoCompras carrinho) {
+		this.carrinho = carrinho;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Cliente)) {
+			return false;
+		}
+		final Cliente usuario = (Cliente) o;
+		return Objects.equals(getId(), usuario.getId()) && Objects.equals(getNome(), usuario.getNome()) && Objects.equals(getSobrenome(), usuario.getSobrenome()) && Objects.equals(getCarrinho(), usuario.getCarrinho());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getId());
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario{" +
+				"id='" + id + '\'' +
+				", nome='" + nome + '\'' +
+				", sobrenome='" + sobrenome + '\'' +
+				", carrinho=" + carrinho +
+				'}';
+	}
+}
